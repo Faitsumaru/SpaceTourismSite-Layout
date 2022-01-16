236eed743a44226752938f9895e7be576f357b51
Gleb <kgm20011505@mail.ru>
2022-01-16 14:10:15 +0300

    5 block <info> 3/3 (slider) is done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(function(){
 
+    $('.slider__box').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 10000,
+        prevArrow: '<button type="button" class="slider-prev"><img src="images/arrow-left-slider.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slider-next"><img src="images/arrow-right-slider.png" alt=""></button>',
+        fade: true
+    });
+
+
+
+
 });
